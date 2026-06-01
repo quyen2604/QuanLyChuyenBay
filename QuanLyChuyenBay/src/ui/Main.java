0f4fa8159a14bad00c9ff8b76c8449b871046df9
Dang Huynh Quyen <24130258@st.hcmuaf.edu.vn>
2026-06-01 09:04:09 +0700

    add main

--- QuanLyChuyenBay/src/ui/Main.java
+++ QuanLyChuyenBay/src/ui/Main.java
@@ -3,5 +3,6 @@ package ui;
 public class Main {
 		public static void main(String[] args) {
 		System.out.println("=== Chương trình quản lý chuyến bay ===");
+		System.out.println("1. Quản lý chuyến bay");
 	}
 }
